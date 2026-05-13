a4ef644e545dda34fabe7db1a1cbddac26db48aa
Gaspard Brousaud <154594915+gaspardbroussaud@users.noreply.github.com>
2026-05-13 11:43:48 +0200

    Is it going to fix it

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'recettes-v4';
+const CACHE = 'recettes-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
