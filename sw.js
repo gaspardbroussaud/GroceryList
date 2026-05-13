14fe8f6a2ef32c0c2688bf960a597dc73b0e99f7
Gaspard Brousaud <154594915+gaspardbroussaud@users.noreply.github.com>
2026-05-13 12:36:44 +0200

    Please

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'recettes-v7';
+const CACHE = 'recettes-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
